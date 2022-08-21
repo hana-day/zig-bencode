590cd0912cfddf468e85d415fca93ac4e4fa1b70
Yusuke Hanada <hyusuk9872@gmail.com>
2022-08-21 16:40:41 +0900

    Implement parsing bytestring

--- src/main.zig
+++ src/main.zig
@@ -3,19 +3,31 @@ const testing = std.testing;
 
 const Error = error{
     InvalidInteger,
-    InvalidCharacter,
+    InvalidByteString,
+
+    UnexpectedCharacter,
     UnexpectedToken,
     UnexpectedEnd,
 };
 
-const Token = union(enum) { Integer: struct {
-    i: usize,
-    count: usize,
+const Token = union(enum) {
+    Integer: struct {
+        i: usize,
+        count: usize,
 
-    pub fn slice(self: @This(), input: []const u8) []const u8 {
-        return input[self.i .. self.i + self.count];
-    }
-} };
+        pub fn slice(self: @This(), input: []const u8) []const u8 {
+            return input[self.i .. self.i + self.count];
+        }
+    },
+    ByteString: struct {
+        i: usize,
+        count: usize,
+
+        pub fn slice(self: @This(), input: []const u8) []const u8 {
+            return input[self.i .. self.i + self.count];
+        }
+    },
+};
 
 const TokenStream = struct {
     i: usize = 0,
@@ -31,7 +43,8 @@ const TokenStream = struct {
         if (self.i < self.slice.len) {
             switch (self.slice[self.i]) {
                 'i' => return self.nextInteger(),
-                else => return Error.InvalidCharacter,
+                '0'...'9' => return self.nextByteString(),
+                else => return Error.UnexpectedCharacter,
             }
         }
         return null;
@@ -86,12 +99,36 @@ const TokenStream = struct {
             .count = self.i - tok_begin - 1,
         } };
     }
+
+    fn nextByteString(self: *@This()) Error!?Token {
+        const len_begin = self.i;
+        try self.readInteger(':');
+        self.i += 1; // skip ':'
+        const count = std.fmt.parseInt(usize, self.slice[len_begin .. self.i - 1], 10) catch {
+            return Error.InvalidByteString;
+        };
+        if (self.i + count > self.slice.len) {
+            return Error.UnexpectedEnd;
+        }
+        const tok_begin = self.i;
+        self.i += count;
+        return Token{ .ByteString = .{
+            .i = tok_begin,
+            .count = count,
+        } };
+    }
 };
 
 fn ParseError(comptime T: type) type {
     switch (@typeInfo(T)) {
         .Int, .ComptimeInt => {
             return Error || std.fmt.ParseIntError;
+        },
+        .Array => {
+            return Error;
+        },
+        .Pointer => {
+            return Error;
         },
         else => return error{},
     }
@@ -107,8 +144,21 @@ fn parse(comptime T: type, slice: []const u8) ParseError(T)!T {
                 .Integer => |integerToken| {
                     return std.fmt.parseInt(T, integerToken.slice(slice), 10);
                 },
+                else => return Error.UnexpectedToken,
             }
         },
+        .Array => |arrayInfo| {
+            switch (token) {
+                .ByteString => |byteStringToken| {
+                    if (arrayInfo.child != u8) return Error.UnexpectedToken;
+                    var r: T = undefined;
+                    std.mem.copy(u8, &r, byteStringToken.slice(slice));
+                    return r;
+                },
+                else => return Error.UnexpectedToken,
+            }
+        },
+
         else => @compileError("Unable to parse into type '" ++ @typeName(T) ++ "'"),
     }
     unreachable;
@@ -121,18 +171,33 @@ test "tokenize" {
         try testing.expect(token.? == Token.Integer);
         try testing.expectEqualStrings("1", token.?.Integer.slice(ts.slice));
     }
+    {
+        var ts = TokenStream.init("4:spam");
+        const token = try ts.next();
+        try testing.expect(token.? == Token.ByteString);
+        try testing.expectEqualStrings("spam", token.?.ByteString.slice(ts.slice));
+    }
 }
 
 test "parsing" {
-    try testing.expect(0 == (try parse(u8, "i0e")));
-    try testing.expect(1 == (try parse(u8, "i1e")));
-    try testing.expect(255 == (try parse(u8, "i255e")));
-    try testing.expect(-1 == (try parse(i8, "i-1e")));
-    try testing.expect(-127 == (try parse(i8, "i-127e")));
+    {
+        try testing.expect(0 == (try parse(u8, "i0e")));
+        try testing.expect(1 == (try parse(u8, "i1e")));
+        try testing.expect(255 == (try parse(u8, "i255e")));
+        try testing.expect(-1 == (try parse(i8, "i-1e")));
+        try testing.expect(-127 == (try parse(i8, "i-127e")));
 
-    try testing.expectError(Error.InvalidInteger, parse(u8, "ie"));
-    try testing.expectError(Error.InvalidInteger, parse(u8, "i01e"));
-    try testing.expectError(Error.InvalidInteger, parse(i8, "i-e"));
-    try testing.expectError(Error.InvalidInteger, parse(i8, "i-0e"));
-    try testing.expectError(Error.UnexpectedEnd, parse(u8, "i1"));
+        try testing.expectError(Error.InvalidInteger, parse(u8, "ie"));
+        try testing.expectError(Error.InvalidInteger, parse(u8, "i01e"));
+        try testing.expectError(Error.InvalidInteger, parse(i8, "i-e"));
+        try testing.expectError(Error.InvalidInteger, parse(i8, "i-0e"));
+        try testing.expectError(Error.UnexpectedEnd, parse(u8, "i1"));
+    }
+    {
+        try testing.expectEqual([0]u8{}, (try parse([0]u8, "0:")));
+        try testing.expectEqual([4]u8{ 's', 'p', 'a', 'm' }, (try parse([4]u8, "4:spam")));
+
+        try testing.expectError(Error.InvalidInteger, parse([4]u8, "01:spam"));
+        try testing.expectError(Error.UnexpectedEnd, parse([4]u8, "1:"));
+    }
 }
